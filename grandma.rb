efdc8a4208645db27be71f645ccf6ecc898a6ba0
Aaron Wilson <codingsensei19@gmail.com>
2020-01-24 01:51:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,9 @@ if Whatever you say to grandma, she should respond with
 puts HUH?! SPEAK UP, SONNY!
 end
 
-# If you shout, she can hear you (or at least she thinks so)
-# and yells back
-
-# NO, NOT SINCE 1938!
+if If you shout, she can hear you (or at least she thinks so)
+puts NO, NOT SINCE 1938!
+end
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
